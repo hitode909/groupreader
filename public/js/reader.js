173f56292346c5ecd2b5633264cf6e2f95e1b472
hitode909 <hitode909@gmail.com>
2009-08-18 18:33:02 +0900

    design, bug fix.

--- public/js/reader.js
+++ public/js/reader.js
@@ -1,13 +1,14 @@
 $(document).ready(
     function(){
-        var box = $("div.reader");
+        var box = $("div.items");
         var uri = ["/group", Feeds.group, "feeds.json"].join("/");
         $.getJSON(uri, {}, function(data) {
             $(data).each(function(){
                 $.getJSON("/feed/get.json", {uri: this.uri}, function(data) {
                     $(data.items).each(function(){
                         var item = $("<div>").addClass("item").append($("<span>").append($("<a>").attr("href", this.link).text(this.title)));
-                        item.append($("<div>").addClass("item-body").append($(this.description)));
+                        var body = $(this.description).length ? $(this.description) : document.createTextNode(this.description);
+                        item.append($("<div>").addClass("item-body").append(body));
                         box.append(item);
                     });
                 });
