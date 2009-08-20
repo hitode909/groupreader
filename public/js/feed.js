3e7e5f7f8a64e6d8711936f15bb29455d460a6f2
hitode909 <hitode909@gmail.com>
2009-08-20 18:35:46 +0900

    ajax subscribe(callback is not yet).

--- public/js/feed.js
+++ public/js/feed.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
-    $('.newfeed').click(function() {
-        $(this).unbind('click');
-        this.innerHTML = '<form method="POST" action="/group/' + GroupReader.group + '/subscribe"><input name="feed_uri" type="text" size="50"></input></form>';
+    $('.newfeed .new-button').click(function() {
+        $('.newfeed .new-button').hide();
+        $('.newfeed .new-input').show();
+    });
+
+    $('.newfeed form').submit(function() {
+        var feed_uri = $("input[name='feed-uri']")[0].value;
+        if (!feed_uri) return false;
+        $.post('/api/group/subscribe',
+               { feed_uri: feed_uri,
+                 name: GroupReader.group
+               },
+               function(data){
+                   console.log(data);
+               },
+               'json'
+              );
+
+        $('.newfeed .new-button').show();
+        $('.newfeed .new-input').hide();
+        $("input[name='feed-uri']")[0].value = '';
+
+        return false;
     });
 });
