64694a1be29a7c49eb7d1e4df6d52cde7bff3ba1
hitode909 <hitode909@gmail.com>
2009-08-23 16:16:17 +0900

    not used rule

--- controller/init.rb
+++ controller/init.rb
@@ -12,12 +12,3 @@ end
 
 require 'controller/main'
 require 'controller/api'
-
-Innate::Route['group_entity'] = lambda{ |path, request|
-   if path =~ %r|^/group/([^/.]+)\/([^/.]+)$|
-     "/group/#{$2}/#{$1}"
-   elsif path =~ %r|^/group/([^/.]+)\/([^/.]+)\.([^/.]+)$|
-     "/group/#{$2}/#{$1}.#{$3}"
-   end
-}
-
